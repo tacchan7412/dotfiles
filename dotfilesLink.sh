df9b9f67633d2bbc43d37c08c7e87373bf0cae78
tacchan7412 <tacchan7412@DSM1.local>
2018-04-04 17:55:09 +0900

    fix shell file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-mkdir -p ~/.vim/colors
+mkdir -p ~/.vim
 ln -sf ~/dotfiles/colors ~/.vim/colors
+mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
