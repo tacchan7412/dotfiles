e9c1ce9cb0c5864c54f5da696e7f590f727ecc6c
tacchan7412 <tacchan.04@gmail.com>
2018-06-02 00:12:59 +0900

    add .zshrc and fix sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,28 @@
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-mkdir -p ~/.vim
-ln -sf ~/dotfiles/colors ~/.vim/colors
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
+vimdir=~/.vim
+colordir=~/.vim/colors
+deindir=~/.vim/dein/repos/github.com/Shougo/dein.vim
+
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+if [ -e $vimdir ]; then
+    echo "vimdir already exists."
+else 
+    mkdir -p $vimdir
+fi
+
+if [ -e $colordir ]; then
+    echo "colordir already exists."
+else 
+    mkdir -p $colordir
+fi
+
+ln -s ~/dotfiles/colors/* ~/.vim/colors
+
+if [ -e $deindir ]; then
+    echo "deindir already exists"
+else
+    mkdir -p deindir
+    git clone https://github.com/Shougo/dein.vim.git deindir
+fi
+
